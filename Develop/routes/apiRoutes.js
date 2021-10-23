463682764fbefd14e10f65678902f4cd409bf716
ObeeK <chasenharvey@gmail.com>
2021-10-23 11:22:32 -0500

    finished

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,29 +1,45 @@
 // don't need to add /api/... just need /...
 
-const router = require('express').Router();
+// const router = require('express').Router();
 const path = require('path');
 const fs = require("fs");
 const db = path.resolve(__dirname, "../db")
+const router = require('express').Router();
 let idNumber = 1;
 
-router.get('/notes', (req, res) => {
-    fs.readFile(path.resolve(db, "db.json"), (err, data) => {
-        res.json(JSON.parse(data));
+
+    console.log(__dirname);
+    router.get('/notes', (req, res) => {
+        fs.readFile(path.resolve(db, "db.json"), (err, data) => {
+            res.json(JSON.parse(data));
+        });
     });
-});
 
-router.post("/notes", (req, res) => {
-    let notesArray = [];
+    router.post("/notes", (req, res) => {
+        let notesArray = [];
 
-    let data = fs.readFileSync(path.resolve(db, "db.json"));
-    notesArray = JSON.parse(data);
+        let data = fs.readFileSync(path.resolve(db, "db.json"));
+        notesArray = JSON.parse(data);
 
-    let newNotesArray = {
-        id: idNumber,
-        title: req.body.title,
-        text: req.body.text,
-    };
+        let newNotesArray = {
+            id: idNumber,
+            title: req.body.title,
+            text: req.body.text,
+        };
 
-    
-    
-})
+        notesArray.push(newNotesArray);
+
+        fs.writeFileSync(
+            path.resolve(db, "db.json"),
+            JSON.stringify(notesArray),
+            function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            }
+        );
+        res.json(newNotesArray);
+        idCounter += 1;
+    });
+
+module.exports = router
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 const path = require('path');
 
+// module.exports = (app) => {
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+    router.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
+    router.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
 
+    // app.get('/notes', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '../public/notes.html'));
+    // });
+// };
 module.exports = router
